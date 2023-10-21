ce390f3c3d75b13f62a8d2b57cadf4df2301acb8
gcolomer <gcolomer@protonmail.com>
2023-10-21 14:25:07 +0200

    Add user and prisma module Add prisma service (create instance) Add schema prisma (model of data) Add auth dto class (need validation-class and transformer-class) with index.ts for export Import prisma in auth for register user Add signup and signin function in AuthService Fix docker-compose port Add Validation pipe for use validation decorator, with whitelist for protect code to injection

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,12 +1,29 @@
 import { Injectable } from '@nestjs/common'
+import { PrismaService } from "../prisma/prisma.service";
+import {AuthDto} from "./dto";
+import * as argon from 'argon2'
 
 @Injectable({})
 export class AuthService{
-    signgin() {
-        return { msg: 'I have signed in' }
+    constructor(private prisma: PrismaService) {
     }
 
-    signup() {
-        return { msg: 'I have sign up' }
+    async signup(dto: AuthDto) {
+        // generate the hash password
+        const hash = await argon.hash(dto.password);
+        // save user in db
+        const user = await this.prisma.user.create({
+            data: {
+                email: dto.email,
+                password: hash,
+            },
+        })
+        delete user.password;
+        // return the save user
+        return user;
+    }
+
+    signgin() {
+        return { msg: 'I have signed in' }
     }
 }
