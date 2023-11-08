b66969f2de3990fae2049b86596d26edbd84de51
gcolomer <gcolomer@protonmail.com>
2023-11-08 11:10:37 +0100

    Add chat composant to home

--- front/src/components/Chat.tsx
+++ front/src/components/Chat.tsx
@@ -1,27 +1,36 @@
+// @ts-ignore
 import React from 'react';
 import { useState } from 'react';
+import WindowChat from "./WindowChat";
 
 function Chat(): React.FC {
     const [chatIsOpen, setChatIsOpen] = useState(false);
+    const [windowIsOpen, setWindowIsOpen] = useState(false);
 
     const toggleChat = () => {
         setChatIsOpen(chatIsOpen !== true)
     }
 
+    const toggleWindow = () => {
+        setWindowIsOpen(windowIsOpen !== true)
+    }
     return (
-        <div className="drawer drawer-end flex-col-reverse h-full items-end">
+        <div className="drawer drawer-end flex flex-col-reverse h-full items-end">
             <input id="my-drawer-4" type="checkbox" className="drawer-toggle justify-center" />
             <div className="drawer-content">
                 {/* Page content here */}
                 <label htmlFor="my-drawer-4" className="drawer-button btn btn-circle m-5 text-orangeNG text-xs font-display p-7 ">chat</label>
             </div>
-            <div className="drawer-side mt-16">
+            <div className="drawer-side mt-16 flex flex-row-reverse items-end">
                 <label htmlFor="my-drawer-4" aria-label="close sidebar" className="drawer-overlay opacity-0"></label>
-                <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+                <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content ">
                     {/* Sidebar content here */}
-                    <li><a>Sidebar Item 1</a></li>
-                    <li><a>Sidebar Item 2</a></li>
+                    <li><button className="btn btn-ghost font-display text-orangeNG" onClick={toggleWindow}>User 1</button></li>
+                    <li><button className="btn btn-ghost font-display text-orangeNG" onClick={toggleWindow}>User 2</button></li>
                 </ul>
+                {windowIsOpen && (
+                   <WindowChat/>
+                )}
             </div>
         </div>
     );
